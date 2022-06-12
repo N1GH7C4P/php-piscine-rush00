a772ae484e09ffa0ca111f75e0524a8e063abb71
N1GH7C4P <kimmo.polojarvi@gmail.com>
2022-06-12 13:40:26 +0300

    database rename

--- services/product_service.php
+++ services/product_service.php
@@ -25,11 +25,42 @@
 		$connection->close();
 	}
 
-	
+	function increase_inventory($product_id, $quantity)
+	{
+		$connection = connect_to_database();
+		$query = "UPDATE `products` SET `quantity`=`quantity`+".$quantity." WHERE `id`=".$product_id;
+		$connection->query($query);	
+		$connection->close();
+	}
+
+	function get_nb_of_product_in_user_basket($user_id, $product_id)
+	{
+		$connection = connect_to_database();
+		$query = "SELECT `basket_item`.`quantity` FROM `basket_item`
+			LEFT JOIN `basket` ON `basket_item`.`basket_id`=`basket`.`id`
+			LEFT JOIN `products` ON `products`.`id`=`basket_item`.`product_id`
+			LEFT JOIN `users` ON `users`.`id`=`basket`.`user_id`
+				WHERE `users`.`id`=".$user_id." AND `products`.`id`=".$product_id.";";
+		$res = $connection->query($query);
+		$connection->close();
+		if(!$res)
+			return NULL;
+		while ($obj = $res->fetch_object())
+			return $obj->quantity;
+	}
+
 	function create_basket_item($basket_id, $product_id, $quantity)
 	{
 		$connection = connect_to_database();
 		$query = "INSERT INTO `basket_item` (`id`, `basket_id`, `product_id`, `quantity`, `creation_time`) VALUES (NULL, ".$basket_id.", ".$product_id.", ".$quantity.", current_timestamp());";
+		$connection->query($query);
+		$connection->close();
+	}
+
+	function remove_basket_item($basket_id, $product_id)
+	{
+		$connection = connect_to_database();
+		$query = "DELETE * FROM `basket_item` WHERE `basket_id=`".$basket_id."AND `product_id=`".$product_id.";";
 		$connection->query($query);
 		$connection->close();
 	}
@@ -42,9 +73,29 @@
 		$connection->close();
 	}
 
+	function update_basket_price($basket_id, $sum)
+	{
+		$connection = connect_to_database();
+		$query = "UPDATE `basket` SET `price_total` = ".$sum." WHERE `basket`.`id` = ".$basket_id.";";
+		$connection->query($query);
+		$connection->close();
+	}
+
+	function remove_product_from_users_basket($login, $product_id)
+	{
+		$user_id = get_id_by_login($login);
+		$basket_obj = get_user_basket($user_id);
+		if(!$basket_obj)
+			return -1;
+		$basket_id = $basket_obj->id;
+		$quantity = get_nb_of_product_in_user_basket($user_id, $product_id);
+		remove_basket_item($basket_id, $product_id);
+		if($quantity)
+			increase_inventory($product_id, $quantity);
+	}
+
 	function add_product_to_users_basket($login, $product_id, $quantity)
 	{
-		echo("adding product\n");
 		$user_id = get_id_by_login($login);
 		$basket_obj = get_user_basket($user_id);
 		if(!$basket_obj)
@@ -52,8 +103,10 @@
 			create_basket($user_id);
 			$basket_obj = get_user_basket($user_id);
 		}
-		print_r($basket_obj);
 		$basket_id = $basket_obj->id;
+		$product_price = get_product_price($product_id);
+		//echo("nb: ".$quantity." unit_price: ".$product_price." total: ".$quantity * $product_price);
+		update_basket_price($basket_id, $quantity * $product_price);
 		create_basket_item($basket_id, $product_id, $quantity);
 		remove_from_inventory($product_id, $quantity);
 	}
@@ -77,7 +130,7 @@
 		$query = "SELECT * FROM `basket` WHERE `user_id`=".$id.";";
 		$res = $connection->query($query);
 		if(!$res)
-			return -1;
+			return NULL;
 		while ($obj = $res->fetch_object())
 			return $obj;
 	}
@@ -92,12 +145,10 @@
 		WHERE `users`.`id`=".$user_id.";";
 		$res = $connection->query($query);
 		if(!$res)
-			return -1;
+			return NULL;
 		$i = 0;
 		while ($obj[$i] = $res->fetch_object())
-		{
 			$i++;
-		}
 		return $obj;
 	}
 
@@ -125,6 +176,18 @@
 			return -1;
 		while ($obj = $res->fetch_object())
 			show_product($obj);
+	}
+
+	function get_product_price($id)
+	{
+		$connection = connect_to_database();
+		$query = "SELECT `price` FROM `products` WHERE `id`=".$id.";";
+		$res = $connection->query($query);
+		$connection->close();
+		if(!$res)
+			return -1;
+		while ($obj = $res->fetch_object())
+			return($obj->price);
 	}
 
 	function get_product_by_id($id)
